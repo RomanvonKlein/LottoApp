7a18732b7338fbc890c4b6f71de1caae9c67858a
set <set>
2023-12-10 23:40:43 +0100

    Initial commit. Gradle setup plus Logging and basic structure.

---
+++ app/src/main/java/lottoApp/games/Eurojackpot.java
@@ -0,0 +1,19 @@
+package lottoApp.games;
+
+import java.util.List;
+
+public class Eurojackpot implements IGame{
+
+    @Override
+    public String getGameName() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getGameName'");
+    }
+
+    @Override
+    public List<Integer> getPredictions() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getPredictions'");
+    }
+    
+}
---
+++ app/src/main/java/lottoApp/games/IGame.java
@@ -0,0 +1,8 @@
+package lottoApp.games;
+
+import java.util.List;
+
+public interface IGame {
+    public String getGameName();
+    public List<Integer> getPredictions();
+}
